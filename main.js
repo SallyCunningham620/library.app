31c8d55b9332b7a8d7f61f418c1dec5e082127de
Sally <143855164+SallyCunningham620@users.noreply.github.com>
2025-03-11 14:25:43 -0500

    form submit create a new card with all information read/unread change remove btn on card

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 let newFormBtn = document.getElementById("new-form-btn");
+let formDiv = document.getElementById("form-div");
 let newBookForm = document.getElementById("new-book-form");
 let closeFormBtn = document.getElementById("close-form-btn");
+//let addBookBtn = document.getElementById("add-book-btn");
+let booksContainer = document.querySelector(".books-container");
+const myLibrary = [];
 
 newFormBtn.addEventListener('click', openForm);
 
 function openForm() {
   newBookForm.classList.add("open-new-form");
-  console.log("ouch");
+  console.log("hi");
 }
 
 closeFormBtn.addEventListener('click', closeForm);
@@ -15,23 +19,111 @@ function closeForm() {
   newBookForm.classList.remove("open-new-form");
 }
 
-const myLibrary = [];
+let bookCheck = {
+  bookReadStatus: function() {
+    if (this.unreadOrRead === "Yes") {
+      this.unreadOrRead = "No";
+    } else {
+      this.unreadOrRead = "Yes";
+    }
+    console.log(myLibrary);
+  }
+}
 
-function Book(title, author, pages, readBook) {
+function createBook(title, author, numberPages, unreadOrRead) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
-    this.readBook = this.readBook;
-    this.info = function() {
-      return(this.title + " by " + this.author + " , " + this.pages + " pages, " + readBook);
+    this.numberPages = numberPages;
+    this.unreadOrRead = unreadOrRead
+    this.id = crypto.randomUUID;
+    Object.setPrototypeOf(this, bookCheck);
     };
-  };
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
-
-console.log(theHobbit.info()); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
-
-
-function addBookToLibrary() {
-    // take params, create a book then store it in the array
+function addBookToLibrary(title, author, numberPages, unreadOrRead) {  
+    let book = new createBook(title, author, numberPages, unreadOrRead);
+    myLibrary.push(book);
+    createBookCard(book);
+    console.log(myLibrary);
+    return myLibrary;
   }
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "Yes");
+addBookToLibrary("The Fellowship of the Ring", "J.R.R. Tolkien", 398, "Yes");
+//addBookToLibrary("The Two Towers", "J.R.R. Tolkien", 327, "Yes");
+
+function createBookCard(book) {
+    console.log("Create book card");
+  
+    let bookCard = document.createElement("div");
+    bookCard.classList.add("card");
+    bookCard.id = book.id;
+    booksContainer.appendChild(bookCard);
+  
+    let bookTitle = document.createElement("p");
+    bookTitle.classList.add("book-title");
+    bookTitle.textContent = book.title;
+    bookCard.appendChild(bookTitle);
+  
+    let author = document.createElement("p");
+    author.classList.add("author");
+    author.textContent = "by " + book.author;
+    bookCard.appendChild(author);
+  
+    let numberPages = document.createElement("p");
+    numberPages.classList.add("number-pages");
+    numberPages.textContent = book.numberPages + " pages";
+    bookCard.appendChild(numberPages);
+  
+    let bookCardButtonsContainer = document.createElement("div");
+    bookCardButtonsContainer.classList.add("book-card-buttons-container");
+    bookCard.appendChild(bookCardButtonsContainer);
+  
+    let unreadOrRead = document.createElement("button");
+    unreadOrRead.classList.add("book-read-button");
+    if (book.unreadOrRead === "Yes") {
+      unreadOrRead.classList.add("read-button");
+      unreadOrRead.textContent = "Read";
+    } else {
+      unreadOrRead.classList.add("read-button");
+      unreadOrRead.textContent = "Unread";
+    }
+    unreadOrRead.addEventListener("click", () => {
+      book.bookReadStatus();
+      if (unreadOrRead.classList.contains("read-button")) {
+        unreadOrRead.classList.remove("read-button");
+        unreadOrRead.classList.add("unread-button");
+        unreadOrRead.textContent = "Unread";
+      } else {
+        unreadOrRead.classList.remove("unread-button");
+        unreadOrRead.classList.add("read-button");
+        unreadOrRead.textContent = "Read";
+      }
+    });
+    bookCardButtonsContainer.appendChild(unreadOrRead);
+
+    let bookRemoveButton = document.createElement("button");
+    bookRemoveButton.classList.add("book-card-remove-button");
+    bookRemoveButton.textContent = "Remove";
+	  bookRemoveButton.addEventListener("click", () => {
+		  bookCard.remove();
+  		myLibrary.splice(myLibrary.indexOf(book), 1);
+	  	console.log(myLibrary);
+	  });
+	  bookCardButtonsContainer.appendChild(bookRemoveButton);
+  }
+
+
+  newBookForm.addEventListener("submit", (event) => {
+    let title = document.querySelector("#book-title").value;
+    let author = document.querySelector("#author").value;
+    let numberPages = document.querySelector("#number-pages").value;
+    let unreadOrRead = document.querySelector("#unread-or-read");
+    if (unreadOrRead.checked) {
+      unreadOrRead = "Yes";
+    } else {
+      unreadOrRead = "No";
+    }
+    addBookToLibrary(title, author, numberPages, unreadOrRead);
+    event.preventDefault();
+    newBookForm.classList.remove("open-new-form");
+  });
